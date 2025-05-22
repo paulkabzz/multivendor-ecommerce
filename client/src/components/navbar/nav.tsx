32c4e21a8495804fc7018c691e467c16c4f33ed7
paulkabzz <pkabulu05@gmail.com>
2025-05-22 18:53:48 +0200

    updated nav.tsx

--- client/src/components/navbar/nav.tsx
+++ client/src/components/navbar/nav.tsx
@@ -1,10 +1,4 @@
-import { categories } from "../../utils/categories";
-
 export const Nav = () => {
-  const capitalise = (value: string): string => {
-    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
-  };
-
   return (
     <header className="sticky top-0 right-0 bg-gray-400 w-full mt-[3vh] px-[100px] py-[.5rem] flex flex-col">
       <div className="flex items-center justify-between"></div>
