a9daa8204497c8798a5c0900b54ef23754231281
paulkabzz <pkabulu05@gmail.com>
2025-05-22 20:21:08 +0200

    created routing file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router";
 import "./app.css";
-import Home from "./pages/home/page";
+import { Home } from "./routes";
 import RootLayout from "./root-layout";
 
 function App(): React.ReactElement {
