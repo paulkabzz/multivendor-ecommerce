23754a0cc1dfe98dc20ef20f49dae7fc9edff9b9
paulkabzz <pkabulu05@gmail.com>
2025-05-22 11:16:15 +0200

    added custom fonts

---
+++ client/src/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <>
+            <header className="absolute top-0 right-0 w-full h-[7vh] bg-slate-100">
+                Hello
+            </header>
+        </>
+    );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "main": "./src/assets/fonts/SFPRODISPLAYREGULAR.OTF",
+      }
+    },
   },
   plugins: [],
 }
