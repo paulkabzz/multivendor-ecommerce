b2c8b3010e01fcaf48b1350e75a312dfd0a0414a
paulkabzz <pkabulu05@gmail.com>
2025-05-24 09:21:20 +0200

    created shopping card component

---
+++ client/src/components/common/shopping-card/shopping-card.tsx
@@ -0,0 +1,11 @@
+export const ShoppingCard = () => {
+  return (
+   <>
+        <div className="w-[200px] h-[350px] flex flex-col gap-[.75rem] pb-[.5rem]">
+            <div className="w-full h-[250px] relative overflow-hidden">
+                <img src="" alt="" />
+            </div>
+        </div>
+   </>
+  );
+};
