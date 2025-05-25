16f3010cf83563b0ffe9d1782605fd0aa726e41f
paulkabzz <pkabulu05@gmail.com>
2025-05-25 02:55:14 +0200

    exported pages from routes.ts

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,5 +1,27 @@
+import Activation from '@pages/activation/page';
+import BestSelling from '@pages/best-selling/page';
+import Checkout from '@pages/checkout/page';
+import Contact from '@pages/contact/page';
+import Events from '@pages/events/page';
+import FAQ from '@pages/faq/page';
 import Home from '@pages/home/page';
+import Login from '@pages/login/page';
+import ProductDetails from '@pages/product-details/page';
+import Products from '@pages/products/page';
+import Profile from '@pages/profile/page';
+import SignUp from '@pages/sign-up/page';
 
 export {
-    Home
+    Activation,
+    BestSelling,
+    Checkout,
+    Contact,
+    Events,
+    FAQ,
+    Home,
+    Login,
+    ProductDetails,
+    Products,
+    Profile,
+    SignUp
 };
