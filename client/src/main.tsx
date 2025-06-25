33620b47f57b2f59daf3196bfde5f70da5b2ed7b
paulkabzz <pkabulu05@gmail.com>
2025-06-25 02:33:50 +0200

    installed react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,28 @@ import App from "@src/App";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import { store } from "@src/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryCLient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: 2,
+      refetchOnWindowFocus: false, 
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+        <Provider store={store}>
+          <BrowserRouter>
+              <QueryClientProvider client={queryCLient}>
+                <App />
+              </QueryClientProvider>
+          </BrowserRouter>
+        </Provider>  
+
   </StrictMode>,
 );
