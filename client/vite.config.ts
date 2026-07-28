185e2c1aff54c10f2daab6f91eb5b6212fbe9c63
Paul Kabulu <pkabulu05@gmail.com>
2026-07-28 19:36:24 +0200

    set up client deployment configurations

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/multivendor-ecommerce",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./"),
